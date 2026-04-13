357e8e696aeb03bcb58096b3415d3aa148ce1666
Dev <dev@empresascomercial.com>
2026-04-13 22:34:17 +0100

    upd: update service worker and sync final changes

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.kuead8qrks8"
+    "revision": "0.on1epifbgfk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
