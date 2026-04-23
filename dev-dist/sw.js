2e54b7c49bc43fe3a734b1e2d571ccbd5f360f12
Dev <dev@empresascomercial.com>
2026-04-23 16:17:01 +0100

    terminado

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.5cbk5fnttmo"
+    "revision": "0.dbb1ng1g2qg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
